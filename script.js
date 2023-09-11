c94f61e04dce769842cf8ea67e445a2679c16658
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-10 20:54:49 -0700

    added the arrays for lowercase and uppercase characters, numbers and special characters; created the first validation check to ask the user for the number of characters and to validate that it is between 8 and 128.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', "`", "{", "|", "}", "~"];
+  // CAN'T INCLUDE BACKSLASH IN SPECIAL CHARACTERS
+
+  function numOfChars() {
+    chars = window.prompt("How many characters does the password need to be? Please select a number between 8 and 128.")
+    if (chars >= 8 && chars <= 128) {
+      chars = Number(chars);
+      charTypes();
+    } else {
+      window.alert("Please type a number between 8 and 128.")
+      numOfChars();
+    }
+  }
+
+  function charTypes() {
+
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
